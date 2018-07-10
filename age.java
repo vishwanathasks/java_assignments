8fb20200b55b819fa962c8af7c6cebbf27945856
vishwanathasks <41055371+vishwanathasks@users.noreply.github.com>
2018-07-10 16:49:14 +0530

    Add files via upload

---
+++ age.java
@@ -0,0 +1,69 @@
+class myException extends Exception
+    
+{
+
+        public void  myException()
+
+        {
+        
+		System.out.println("My exception is thrown");
+       
+	}
+        
+        
+	public String toString()
+      
+	  {
+        
+		return "Myexception Object: Age cannot be < 18 and cannot be >60";
+       
+	 }
+   
+ }
+    
+
+
+
+public class age
+
+{
+   
+ static int f=0;
+    
+public static void main(String []args)
+    
+{
+         
+        
+ try
+        
+ {
+            
+ int age=Integer.parseInt(args[0]);
+           
+  if( age <18|| age >60)
+                
+throw new myException();
+       
+  }
+         
+         
+catch(myException e)
+         
+{
+             f=1;
+            
+ System.out.println("Exception:" +e);
+             
+        
+ }
+         
+         
+if(f==0)
+           
+ System.out.println("You have entered correct credential ");
+         
+  
+  }
+
+}
